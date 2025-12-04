27bfb857ab81e817fa88d46634d2176d1d668773
Jihun-ust <moon2472@stthomas.edu>
2025-12-04 12:05:07 -0600

    beta_v1

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ function recordDecision(decision) {
 }
 
 acceptBtn.addEventListener("click", () => recordDecision("accept"));
-denyBtn.addEventListener("click", () => recordDecision("deny"));
+denyBtn.addEventListener("click", () => recordDecision("redo insertion"));
 
 nextBtn.addEventListener("click", () => {
   if (currentIndex < IMAGES.length) {
